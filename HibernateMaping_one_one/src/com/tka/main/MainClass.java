e2711661e5ba90d0421dd1399afa281eb1f6f6d5
Anajli Shinde <157095707+anjuShinde14@users.noreply.github.com>
2024-04-30 16:22:33 +0530

    Add files via upload

---
+++ HibernateMaping_one_one/src/com/tka/main/MainClass.java
@@ -0,0 +1,41 @@
+package com.tka.main;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.tka.entity.Laptop;
+import com.tka.entity.Student;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Configuration config=new Configuration();
+		config.configure("hibernate.cfg.xml");
+		config.addAnnotatedClass(Laptop.class);
+		config.addAnnotatedClass(Student.class);
+		
+		SessionFactory factory= config.buildSessionFactory();
+		
+		Session session= factory.openSession();
+		session.beginTransaction();
+		Laptop l=new Laptop("Accer");
+		
+		Student s=new Student();
+		s.setName("Kiran");
+		s.setEmailid("kiran@gmail.com");
+		s.setLaptop(l);
+		
+		session.persist(l);
+		session.persist(s);
+		session.getTransaction().commit();
+		session.close();
+		
+		
+		
+
+	}
+
+}
