eb58be8c1f5ada0668729e95f254eed81cddabb5
Anajli Shinde <157095707+anjuShinde14@users.noreply.github.com>
2024-04-30 16:28:59 +0530

    Add files via upload

---
+++ Hibernat/src/main/java/com/tka/main/Client.java
@@ -0,0 +1,14 @@
+package com.tka.main;
+
+//import java.lang.module.Configuration;
+
+public class Client {
+	public static void main(String[] args) {
+		
+		Configuration configuration=new Configuration();
+		configuration.();
+//		configuration.addAnnotatedClass(Employee.class);
+//		SessionFactory factory=configuration.buildSessionFactory();
+//		
+	}
+}
